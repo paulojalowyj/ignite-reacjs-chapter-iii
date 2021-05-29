b84858c068a7b89d4d1c75d304feb3478b44b6ff
Paulo Jalowyj <paulo.jalowyj@outlook.com>
2021-05-29 18:10:05 -0300

    Verificando usuários duplicados

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query } from 'faunadb'
+
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
     providers: [
@@ -8,5 +12,39 @@ export default NextAuth({
             clientSecret: process.env.GITHUB_SECRET,
             scope: 'read:user'
         }),
-    ]
+    ],
+    callbacks: {
+        async signIn(user, account, profile) {
+            const { email } = user
+
+            try {
+                await fauna.query(
+                    query.If(
+                        query.Not(
+                            query.Exists(
+                                query.Match(
+                                    query.Index('user_by_email'),
+                                    query.Casefold(user.email)
+                                )
+                            )
+                        ),
+                        query.Create(
+                            query.Collection('users'),
+                            { data: { email } }
+                        ),
+                        query.Get(
+                            query.Match(
+                                query.Index('user_by_email'),
+                                query.Casefold(user.email)
+                            )
+                        )
+                    )
+                )
+                return true;
+
+            } catch {
+                return false;
+            }
+        }
+    }
 })
