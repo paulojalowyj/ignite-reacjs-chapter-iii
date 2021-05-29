bea831c2c386d259589bd591bb5e989619606410
Paulo Jalowyj <paulo.jalowyj@outlook.com>
2021-05-29 18:40:37 -0300

    Criando Sessão no Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,14 +1,30 @@
+import { useSession, signIn } from 'next-auth/client'
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
     priceId: string;
 }
 
+//getServerSideProps
+//getStaticProps
+//API routes
+
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+    const [session] = useSession();
+
+    function handleSubscribe() {
+        if (!session) {
+            signIn('github')
+            return;
+        }
+        //Criação da Checkout Session
+        
+    }
     return (
         <button
             type="button"
             className={styles.subscribeButton}
+            onClick={handleSubscribe}
         >
             Subscribe now
         </button>
